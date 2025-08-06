e751fa83b2d9398d89d1a801464c9005dbe766fe
akash2316y <iamakashrabha@gmail.com>
2025-08-06 17:44:30 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,28 +5,12 @@ API_ID = int(os.getenv("API_ID", "21184495"))
 API_HASH = os.getenv("API_HASH", "7238819d51a5280143fc3023a2f1abed")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7478540145:AAGtOR4C6y7-uCmL_c-VorFd5qM8rIPzBDA")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://akashrabha2005:781120@cluster0.pv6yd2f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", "-1002598448174"))
+DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", "-1002829620846"))
 ADMIN_ID = int(os.getenv("ADMIN_ID", "8110231942"))
 LOG_CHNL = int(os.getenv("LOG_CHNL", "-1002764219378"))
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "NothingCopyright") # Without @
-IS_FSUB = bool(os.environ.get("FSUB", True))
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002764219378").split()))
-DATABASE_CHANNEL_LOG = int(os.getenv("DATABASE_CHANNEL_ID", "-1002598448174"))
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "AlwaysToHelpBot") # Without @
+IS_FSUB = bool(os.environ.get("FSUB", False))
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "").split()))
+DATABASE_CHANNEL_LOG = int(os.getenv("DATABASE_CHANNEL_ID", "-1002635795381"))
 FREE_VIDEO_DURATION = int(os.getenv("FREE_VIDEO_DURATION", "500"))
-
-
-'''
-Smile, please... 😊
-Goodbye forever.
-
-Thank you all for your care and support throughout this journey.
-
-Unfortunately, my @dypixx account has been hacked. The hacker is now using it to promote unknown content. Please be cautious, stay alert, and don’t fall for anything they post.
-
-Take care, stay safe — and once again, thank you for everything.
-Goodbye.
-
-— Dypixx
-
-'''
 
