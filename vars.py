b05de543afea0c045a37e9cdfe140f798da9f833
akash2316y <iamakashrabha@gmail.com>
2025-08-06 17:45:09 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,7 +3,7 @@ from typing import List
 
 API_ID = int(os.getenv("API_ID", "21184495"))
 API_HASH = os.getenv("API_HASH", "7238819d51a5280143fc3023a2f1abed")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7478540145:AAGtOR4C6y7-uCmL_c-VorFd5qM8rIPzBDA")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8418453336:AAFzDB525JUiCdFrPWgstt7xV15gX4xfEM4")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://akashrabha2005:781120@cluster0.pv6yd2f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", "-1002829620846"))
 ADMIN_ID = int(os.getenv("ADMIN_ID", "8110231942"))
@@ -13,4 +13,5 @@ IS_FSUB = bool(os.environ.get("FSUB", False))
 AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "").split()))
 DATABASE_CHANNEL_LOG = int(os.getenv("DATABASE_CHANNEL_ID", "-1002635795381"))
 FREE_VIDEO_DURATION = int(os.getenv("FREE_VIDEO_DURATION", "500"))
+
 
